a8667ce9124adcffc27454e44ef975eee804689f
01print <wulgre35pc@163.com>
2019-12-17 11:05:41 +0800

    Update Data_visualisation.R

--- Data_visualisation.R
+++ Data_visualisation.R
@@ -6,3 +6,10 @@ mpg
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy))
+ggplot(data  = mpg)
+?mpg
+### exercises
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cyl))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = class, y= drv))
